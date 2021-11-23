754b76f24b612dc49222f4cbead06ec61aafa626
Pieter Swinkels <swinkels.pieter@yahoo.com>
2021-11-23 21:07:53 +0100

    Allow override of configure step in Makefile.configure
    This commit also removes Makefile.uncompress-cmake-bootstrap, whose rules can
    now be supported by Makefile.configure.

--- packages/Makefile.cmake
+++ packages/Makefile.cmake
@@ -3,7 +3,8 @@
 
 PACKAGE_VERSION=3.20.0
 PACKAGE_URL=https://github.com/Kitware/CMake/releases/download/v$(PACKAGE_VERSION)/cmake-$(PACKAGE_VERSION).tar.gz
-PACKAGE_TYPE=uncompress-cmake-bootstrap
+PACKAGE_TYPE=configure
+PACKAGE_CONFIGURE_COMMAND=./bootstrap --prefix=$(PACKAGE_STOW_PATH)
 PACKAGE_ARCHIVE_DIR=cmake-$(PACKAGE_VERSION)
 PACKAGE_ARCHIVE_TARGET=git
 PACKAGE_STOW_TARGET=bin/cmake
